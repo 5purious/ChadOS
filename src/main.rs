63ee95e89b4850736d1743d16695e041e56b75b5
Ian-M-Moffett <spruiousvector@gmail.com>
2022-06-07 14:29:05 -0400

    refactor: Cleaned up.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! { 
-    let mut screen = screen::Screen::new(0, 0xB);
+    let mut screen = screen::Screen::new(0, screen::Color::Green);
     screen.writestr(&HELLO);
     loop {}
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,12 +1,34 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
 pub struct Screen {
     x: usize,
-    draw_color: u8,
+    draw_color: Color,
     vga: *mut u8
 }
 
 
 impl Screen {
-    pub fn new(ix: usize, draw_color: u8) -> Self {
+    pub fn new(ix: usize, draw_color: Color) -> Self {
         Self { x: ix, draw_color: draw_color, vga: 0xB8000 as *mut u8 }
     }
 
@@ -14,7 +36,7 @@ impl Screen {
         for &byte in str.iter() {
             unsafe {
                 *self.vga.offset(self.x as isize * 2) = byte;
-                *self.vga.offset(self.x as isize * 2 + 1) = self.draw_color;
+                *self.vga.offset(self.x as isize * 2 + 1) = self.draw_color as u8;
             }
 
             self.x += 1;
